474e8c85449d31841c3de97a35f9a96cccaca9d1
mario andre camones patricio <marioandrecamonespatricio@MacBook-Pro-de-mario.local>
2019-03-08 16:41:28 -0500

    vsariable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
 	urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-	urlDB = 'mongodb+srv://marioandre0601:7lpmaXLyq9Lk7kgH@cluster0-hvsco.mongodb.net/cafe?retryWrites=true';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
