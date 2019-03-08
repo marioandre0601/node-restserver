44a405127bb1cc4dae38d4c42d70a7498b55c249
mario andre camones patricio <marioandrecamonespatricio@MacBook-Pro-de-mario.local>
2019-03-08 16:25:54 -0500

    rest server

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
  * PORT
  */
 process.env.PORT = process.env.PORT || 8080;
+
+/**
+ *  ENTORNO
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/**
+ *  BASE DE DATOS
+ */
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+	urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+	urlDB = 'mongodb+srv://marioandre0601:7lpmaXLyq9Lk7kgH@cluster0-hvsco.mongodb.net/cafe?retryWrites=true';
+}
+
+process.env.URLDB = urlDB;
+// "mongodb+srv://cluster0-hvsco.mongodb.net/cafe" 
---
+++ server/routes/usuario.js
@@ -0,0 +1,157 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const app = express();
+const Usuario = require('../model/usuario');
+
+
+app.get('/usuario', function (req, res) {
+
+	let estado = {
+		estado : false
+	};
+
+	let desde = req.query.desde || 0;
+	desde = Number(desde);
+
+	let limite = req.query.limite || 5;
+	limite = Number(limite);
+
+	Usuario.find(estado,'nombre email img role google estado')
+		.skip(desde)
+		.limit(limite)
+		.exec((err,usuarios)=>{
+			if (err) {
+				return res.status(400).json({
+					ok:false,
+					err
+				});
+			}
+
+			Usuario.count(estado,(err,conteo)=>{
+				res.json({
+					ok: true,
+					usuarios,
+					total: conteo
+				});
+			});
+			
+		});
+});
+
+app.post('/usuario', function (req, res) {
+	let body = req.body;
+
+	let usuario = new Usuario({
+		nombre: body.nombre,
+		email: body.email,
+		password: bcrypt.hashSync(body.password,10),
+		role: body.role
+	});
+
+	usuario.save((err,usuarioDB) =>{
+		if (err) {
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+
+		res.json({
+			ok:true,
+			usuario:usuarioDB,
+		});
+	});
+});
+
+app.put('/usuario/:id', function (req, res) {
+	let id = req.params.id;
+	let body= _.pick(req.body,['nombre','email','img','role','estado']);
+
+	Usuario.findByIdAndUpdate( id, body, {new:true , runValidators:true} , (err,usuarioDB) => {
+		if (err) {
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+	// Usuario.findById( id, (err,usuarioDB) => {
+
+		res.json({
+			ok: true,
+			usuario : usuarioDB
+		});
+	});
+
+	
+});
+
+app.delete('/usuario/:id', function (req, res) {
+	let id = req.params.id;
+	let body= _.pick(req.body,['estado']);
+
+	Usuario.findByIdAndUpdate(id,body,{new:true},(err,usuarioBorrado) =>{
+		if (err) {
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+		if (!usuarioBorrado) {
+			return res.status(400).json({
+				ok:false,
+				err:{
+					message:'Usuario no encontrado'
+				}
+			});
+		}
+	// Usuario.findById( id, (err,usuarioDB) => {
+
+		res.json({
+			ok: true,
+			usuario : usuarioBorrado
+		});
+	});
+
+	// Usuario.findByIdAndRemove(id,(err,usuarioBorrado) =>{
+	// 	if (err) {
+	// 		return res.status(400).json({
+	// 			ok:false,
+	// 			err
+	// 		});
+	// 	}
+	// 	if (!usuarioBorrado) {
+	// 		return res.status(400).json({
+	// 			ok:false,
+	// 			err:{
+	// 				message:'Usuario no encontrado'
+	// 			}
+	// 		});
+	// 	}
+
+	// 	res.json({
+	// 		ok: true,
+	// 		usuario : usuarioBorrado
+	// 	});
+
+	// });
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
